3a7baffde1b842f9ca3bdb143b8d15bee38efeb2
Le Thi Thanh Xuan (Hellen)/SGVN QA Team <lexuan.vn@smilegate.com>
2025-12-17 18:50:34 +0700

    17 dec

--- tests/pages/partners_page/official_homepage/register_mod_component_page.py
+++ tests/pages/partners_page/official_homepage/register_mod_component_page.py
@@ -1,28 +1,30 @@
 import uuid
 from playwright.sync_api import Playwright,Page
-class register_feature_mod_component:
+class register_mod_component:
     def __init__(self,page:Page):
         self.page =page
         self.iframe = page.frame_locator("#iframe-BLZ00000001004")
-        self.component_type= self.page.iframe.locator('//*[contains(text(),"Featured MOD")]')
+        self.component_type= self.iframe.locator('//*[contains(text(),"Featured MOD")]')
         self.random_suffix = uuid.uuid4().hex[:6]
         self.title_input= self.iframe.locator('//*[@id="component-title"]')
-        self.display_options= self.iframe.locator()
-        self.display_period= self.iframe.locator()
-        self.display_order= self.iframe.locator()
-        self.use_title_options=self.iframe.locator()
-        self.en_language_component_title= self.iframe.locator()
-        self.set_aggregation_period= self.iframe.locator()
-        self.mod_register_btn= self.iframe.locator()
-        self.mod_type_btn= self.iframe.locator()
-        self.over_lay= self.iframe.locator()
-        self.mod_type_select=self.iframe.locator()
-        self.search_mod_btn= self.iframe.locator()
-        self.mod_checkbox= self.iframe.locator()
-        self.registe_mod_btn=self.iframe.locator()
-        self.register_scs_confirm= self.iframe.locator()
-        self.register_btn= self.iframe.locator()
-        self.register_complete_btn= self.iframe.locator()
+        self.display_options= self.iframe.locator('(//*[@name="display_true"])[1]')
+        self.display_period= self.iframe.locator('//*[@id="component-display-period"]')
+        self.display_order= self.iframe.locator('//*[@id="component-displayOrder"]')
+        self.use_title_options=self.iframe.locator('(//*[@name="display_true"])[2]')
+        self.en_language_component_title= self.iframe.locator('//*[@id="component-title-en"]')
+        self.set_aggregation_period= self.iframe.locator('//*[contains(text(),"Entire")]')
+        self.mod_register_btn= self.iframe.locator('//*[@class="p-button p-component text-14"]')
+        self.mod_type_btn= self.iframe.locator('//*[@id="modType"]')
+        self.over_lay= self.iframe.locator('//*[@class="p-select-overlay p-component"]')
+        self.mod_type_select=self.iframe.locator('//span[text()="Official MOD"]')
+        self.search_mod_btn= self.iframe.locator('//*[@class="p-button-icon i-mdi:magnify text-20"]')
+        self.mod_checkbox= self.iframe.locator('(//*[@class="p-checkbox-input"])[3]')
+        self.registe_mod_btn=self.iframe.locator('(//*[@class="p-button p-component text-14"])[2]')
+        self.register_scs_confirm= self.iframe.locator('//*[contains(text(),"Confirm") and @class="p-button-label"]')
+        self.register_btn= self.iframe.locator('//*[contains(text(),"Register") and @class="p-button-label"]')
+        self.register_complete_btn= self.iframe.locator('//*[contains(text(),"Confirm") and @class="p-button-label"]')
+        #self.new_component= self.iframe.locator()
+
 
 
 
@@ -35,6 +37,7 @@ class register_feature_mod_component:
         self.en_language_component_title.fill(f'EN_feature mod component_{self.random_suffix}')
     def select_mod(self):
         self.mod_register_btn.click()
+        self.mod_type_btn.click()
         self.mod_type_select.click()
         self.search_mod_btn.click()
         self.mod_checkbox.click()
@@ -44,6 +47,9 @@ class register_feature_mod_component:
         self.register_scs_confirm.click()
         self.register_btn.click()
         self.register_complete_btn.click()
+
+    #def verify_register_component_successful(self):
+     #   self.new_component.is_visible()
 
 
 
--- tests/steps/steps_partner/official_homepage/test_register_common_feature_mod_component.py
+++ tests/steps/steps_partner/official_homepage/test_register_common_feature_mod_component.py
@@ -5,10 +5,14 @@ from pytest_bdd.types import GIVEN
 from pytest_bdd import parsers
 
 from tests.pages.partners_page.official_homepage.register_common_component_page import register_common_component
+from tests.pages.partners_page.official_homepage.register_mod_component_page import register_mod_component
 scenarios("../../../features/partners/official_homepage/register_feature_mod_component.feature")
 @pytest.fixture
 def register_common_component_page(page,base_url):
     return register_common_component(page,base_url)
+@pytest.fixture
+def register_mod_component_page(page):
+    return register_mod_component(page)
 @given("User goes to Page component management")
 def selectOfficialHomepage(register_common_component_page):
 
@@ -23,7 +27,22 @@ def selectCountry_Register(register_common_component_page,country:str):
 def select_component_type(register_common_component_page,component_type:str):
     register_common_component_page.select_component_type_page(component_type)
 
-@then ('I can see "Feature Mod" register form')
-def see_component_type_detail(register_common_component_page):
-    register_common_component_page.see_component_type_detail()
+@then (parsers.cfparse('I can see "{component_type}" register form'))
+def see_component_type_detail(register_common_component_page,component_type:str):
+    register_common_component_page.see_component_type_detail(component_type)
 
+@when ('I input all information fields')
+def input_all_fields(register_mod_component_page):
+    register_mod_component_page.input_register_field()
+
+@then('I select mod')
+def select_mod(register_mod_component_page):
+    register_mod_component_page.select_mod()
+
+@then('I register new Feature Mod component successful')
+def verify_register_component_successful(register_mod_component_page):
+    register_mod_component_page.register_mod_components()
+
+#@then('Then I can see "New component" in list')
+#def verify_register_new_component(register_feature_mod_component):
+       # register_feature_mod_component.verify_register_component_successful()
